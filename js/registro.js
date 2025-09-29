2993c8d2e6a73aead76bae0cba5007da145ab86c
Jexson Jair Macalupu Guzman <ext-jexson.macalupu@euroandino.com.pe>
2025-09-29 15:03:13 -0500

    Primer avance

---
+++ js/registro.js
@@ -0,0 +1,41 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  window.location.href = "htm.html";
+});
+
+
+document.getElementById("registroForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+
+  var modal = bootstrap.Modal.getInstance(document.getElementById('registroModal'));
+  modal.hide();
+
+
+  var alerta = document.getElementById("alertaRegistro");
+  alerta.classList.remove("d-none");
+
+
+  setTimeout(() => {
+    alerta.classList.add("d-none");
+  }, 4000);
+});
+document.getElementById("linkRegistro").addEventListener("click", function (event) {
+  event.preventDefault();
+  var loginModal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
+  if (loginModal) loginModal.hide();
+
+  var registroModal = new bootstrap.Modal(document.getElementById('registroModal'));
+  registroModal.show();
+});
+
+function inicarsesion() {
+  const botonopen = document.getElementById("open-logen");
+  const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+
+  botonopen.addEventListener("click", function() {
+    modal.style.display = "block";
+    loginModal.show();
+  });
+}
+
---
+++ js/script.js
@@ -0,0 +1,5 @@
+AOS.init({
+  duration: 1000, // duración de la animación en ms
+  easing: "ease-out",
+  once: false, // 👈 se repetirá cada vez que el elemento aparezca en pantalla
+});
